48b7dbfecafc2aad760ed5a9b4adcca516a8cf13
Dennis Crissman <dcrissma@redhat.com>
2015-08-14 14:47:12 -0400

    remove header at end

--- src/main/java/com/redhat/lightblue/camel/utils/LightblueLockPolicy.java
+++ src/main/java/com/redhat/lightblue/camel/utils/LightblueLockPolicy.java
@@ -53,6 +53,8 @@ public class LightblueLockPolicy implements Policy {
                     processor.process(exchange);
                     lock.release(resourceId);
                 }
+
+                routeContext.getRoute().removeHeader(LOCK_RESOURCE_ID);
             }
         };
 
