52b5875c376d9494596306a3a02da8c59a8589f3
Dennis Crissman <dcrissma@redhat.com>
2015-08-14 13:45:37 -0400

    use Policy instead of TransactionPolicy

--- src/main/java/com/redhat/lightblue/camel/utils/LightblueLockPolicy.java
+++ src/main/java/com/redhat/lightblue/camel/utils/LightblueLockPolicy.java
@@ -5,18 +5,30 @@ import org.apache.camel.Expression;
 import org.apache.camel.Processor;
 import org.apache.camel.model.ExpressionNodeHelper;
 import org.apache.camel.model.ProcessorDefinition;
+import org.apache.camel.model.language.ConstantExpression;
 import org.apache.camel.model.language.ExpressionDefinition;
+import org.apache.camel.spi.Policy;
 import org.apache.camel.spi.RouteContext;
-import org.apache.camel.spi.TransactedPolicy;
 
 import com.redhat.lightblue.client.Locking;
 
-public class LightblueTransaction implements TransactedPolicy {
+/**
+ *
+ *
+ * @author dcrissman
+ */
+public class LightblueLockPolicy implements Policy {
+
+    public final static String LOCK_RESOURCE_ID = "LOCK_RESOURCE_ID";
 
     private final Locking lock;
     private final Expression expression;
 
-    public LightblueTransaction(Locking lock, Expression expression) {
+    /**
+     * @param lock - Lightblue {@link Locking}
+     * @param expression - {@link Expression} for determining the resourceId for a given Exchange.
+     */
+    public LightblueLockPolicy(Locking lock, Expression expression) {
         this.lock = lock;
         this.expression = expression;
     }
@@ -28,16 +40,19 @@ public class LightblueTransaction implements TransactedPolicy {
 
     @Override
     public Processor wrap(final RouteContext routeContext, final Processor processor) {
-        ExpressionDefinition red = ExpressionNodeHelper.toExpressionDefinition(expression);
+        final ExpressionDefinition red = ExpressionNodeHelper.toExpressionDefinition(expression);
         final Expression routeExpression = red.createExpression(routeContext);
 
         return new Processor() {
             @Override
             public void process(Exchange exchange) throws Exception {
-                String resourceId = routeExpression.evaluate(exchange, String.class);
-                lock.acquire(resourceId);
-                processor.process(exchange);
-                lock.release(resourceId);
+                final String resourceId = routeExpression.evaluate(exchange, String.class);
+                routeContext.getRoute().setHeader(LOCK_RESOURCE_ID, new ConstantExpression(resourceId));
+
+                if (lock.acquire(resourceId)) {
+                    processor.process(exchange);
+                    lock.release(resourceId);
+                }
             }
         };
 
